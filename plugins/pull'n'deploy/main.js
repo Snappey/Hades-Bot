c0adedb7bf5bf9bf0187a710348924387786cc15
Snappey <jonathan.nut@gmail.com>
2016-06-07 22:01:37 +0100

    Outputs to chat now

--- plugins/pull'n'deploy/main.js
+++ plugins/pull'n'deploy/main.js
@@ -6,7 +6,7 @@ var exec = require('child_process').exec;
 var chat = require( appDir + "/plugins/chat/main.js");
 
 chat.Add("pullndeploy", 0, function() {
-	console.log("I'll be right back!");
+	bot.sendMessage(bot.channels[0], "I'll be right back!");
 	exec("/root/deploy.sh", function(err,stdout,stdin) {
 		console.log("stdout: " + stdout);
 		console.log("stdin: " + stdin);
