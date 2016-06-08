827046b3d768ecefd2d6c0966eda346aadf5b79b
Snappey <jonathan.nut@gmail.com>
2016-06-08 17:51:43 +0100

    Greeting fix

--- bot.js
+++ bot.js
@@ -76,7 +76,7 @@ function executeChatCommand(string) {
 
 bot.on("ready", function() {
     console.log("Bot is Ready to use!");
-    bot.sendMessage("Whats Up!");
+    bot.sendMessage(bot.channels[0], "Whats Up!");
 });
 
 var _prfx = []; _prfx["/"] = true; _prfx["!"] = true; _prfx["~"] = true; // TODO: Make them configurable from the client
