2409107f16d2f2826acd386269a48a63e5c609e0
Snappey <jonathan.nut@gmail.com>
2016-06-08 17:45:07 +0100

    XKCD Intergration

--- plugins/xkcd/main.js
+++ plugins/xkcd/main.js
@@ -2,11 +2,53 @@ var path = require('path');
 var appDir = path.dirname(require.main.filename);
 var http = require("http");
 var request= require("sync-request");
+var cron = require("node-schedule");
 
 var chat = require( appDir + "/plugins/chat/main.js");
 
+var host = "http://xkcd.com/";
+
 chat.Add("xkcd", 1, function(num) {
 	if (/^\d+$/.test(num)) {
-		return "http://xkcd.com/" + num;
+		try {
+			var res = JSON.parse(request("GET", host + num + "/info.0.json").getBody().toString());
+			var str = "";
+			str += "\n**" + res.safe_title + "**\n";
+			str += "'" + res.alt + "'\n";
+			str += res.img;
+			return str;
+		} catch(err) {
+			return "Error occured! Comic could not be retrieved!"
+		}
+	}
+})
+
+var rule = new cron.RecurrenceRule();
+rule.dayOfWeek = [1, 3, 5];
+rule.hour = 19;
+rule.minute = 30;
+var daily = cron.scheduleJob(rule, function() {
+	try {
+	var res = JSON.parse(request("GET", host + "/info.0.json").getBody().toString());
+	var str = "";
+	str += "\n**" + res.safe_title + "**\n";
+	str += "'" + res.alt + "'\n";
+	str += res.img;
+	bot.sendMessage(bot.channels[0], str);
+	} catch(err) {
+		return "Error occured! Comic could not be retrieved!"
+	}
+})
+
+chat.Add("xkcddaily", 0, function() {
+	try {
+	var res = JSON.parse(request("GET", host + "/info.0.json").getBody().toString());
+	var str = "";
+	str += "\n**" + res.safe_title + "**\n";
+	str += "'" + res.alt + "'\n";
+	str += res.img;
+	bot.sendMessage(bot.channels[0], str);
+	} catch(err) {
+		return "Error occured! Comic could not be retrieved!"
 	}
 })
