7a0828a6c970644bc28f1dabc5bda3920de539de
Snappey <jonathan.nut@gmail.com>
2016-06-10 20:40:07 +0100

    Basic Functionality Added

--- plugins/wolfram/main.js
+++ plugins/wolfram/main.js
@@ -6,17 +6,20 @@ var parseString = require("xml2js").parseString;
 
 var chat = require( appDir + "/plugins/chat/main.js");
 
-var WFAPIKEY = "XKWPAE-Q7XXVQE4PT";
-
+var WFAPIKEY = "API-KEY";
 
 chat.Add("wf", 1, function(str) {
-	if (str == null || str == "") { return "Statement not valid!"; }
-	var res = request("GET", "http://api.wolframalpha.com/v2/query?appid=" + WFAPIKEY + "&input=" + str + "&format=image,plaintext").getBody().toString();
+	strE = encodeURIComponent(str);
+	if (strE == null || strE == "") { return "Statement not valid!"; }
+	var res = request("GET", "http://api.wolframalpha.com/v2/query?appid=" + WFAPIKEY + "&input=" + strE + "&format=image,plaintext").getBody().toString();
 	var xml = parseString(res, {trim: true}, function(err, res) {
 		if (!err) {
-			var str = "";
+			var msg = "";
 			var res = JSON.parse(JSON.stringify(res));
-			console.log(res.queryresult["pod"]);
+			msg += "https://www.wolframalpha.com/input/?i=" + strE + "\n";
+			msg += "**Input**: " + str + "\n";
+			msg += "**Output**: " + res.queryresult["pod"][1]['subpod'][0]["plaintext"];
+			bot.sendMessage(bot.channels[0], msg)
 		}
 	});
 })
