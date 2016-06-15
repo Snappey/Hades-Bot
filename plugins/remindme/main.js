811ded11f0299db3a12f071133baf433cb4192cc
Snappey <jonathan.nut@gmail.com>
2016-06-15 18:51:46 +0100

    Added RemindMe

--- plugins/remindme/main.js
+++ plugins/remindme/main.js
@@ -1,19 +1,48 @@
 var path = require('path');
 var appDir = path.dirname(require.main.filename);
-var cron = require("node-schedule");
 
 var chat = require( appDir + "/plugins/chat/main.js");
+var remindmes = {};
 
-function FormatToDate(str) {
-	console.log(str)
-	var type = str.search("/[a-z]/i")
-	console.log(str, type)	
+function FormatTime(str) {
+	var type = str.substring(str.length-1, str.length);
+	var len = str.substring(0, str.length-1);
+	var time = "";
+	if (type == "s") {
+		time = len * 1000
+	} else if (type == "m") {
+		time = len * 1000 * 60
+	} else if (type == "h") {
+		time = len * 1000 * 60 * 60
+	} else
+	{
+		time = len * 1000 * 60; // Default to minutes if the time format isnt given
+		bot.sendMessage(bot.channels[0], "Invalid or no time format stated (s,m,h)! Default to minutes!" );
+	}
+	return time;
 }
 
 chat.Add("remind", 1, function(msg) {
 	var time = msg.substring(0, msg.indexOf(" "));
 	var msg = msg.substring(msg.indexOf(" "), msg.length)
+	var delay = FormatTime(time);
 
-	FormatToDate(time);
+	remindmes[msg] = delay;
+	setTimeout(function() {
+		console.log("This ran! " + msg + " : " + delay)
+		bot.sendMessage(bot.channels[0], msg)
+	}, delay)
+	bot.sendMessage(bot.channels[0], "RemindMe Scheduled!");
 
+})
+
+chat.Add("getreminders", 0, function() {
+	var str = "";
+	for (var k in remindmes) {
+		if (remindmes.hasOwnProperty(k)) {
+			kEsc = k.replace("@", "(@)");
+			str += kEsc + " : " + remindmes[k];
+		}
+	}
+	return str;
 })
