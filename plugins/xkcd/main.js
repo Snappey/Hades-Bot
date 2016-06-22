9f7b9aa9c93d03ed38cfe41c2bc999795f26a6f4
Snappey <jonathan.nut@gmail.com>
2016-06-22 13:52:42 +0100

    Changed posting time

--- plugins/xkcd/main.js
+++ plugins/xkcd/main.js
@@ -21,26 +21,26 @@ chat.Add("xkcd", 1, function(num) {
 			return "Error occured! Comic could not be retrieved!"
 		}
 	}
+	else
+	{
+		try {
+		var res = JSON.parse(request("GET", host + "/info.0.json").getBody().toString());
+		var str = "";
+		str += "\n**" + res.safe_title + "**\n";
+		str += "'" + res.alt + "'\n";
+		str += res.img;
+		return str;
+		} catch(err) {
+			return "Error occured! Comic could not be retrieved!"
+		}
+	}
 })
 
 var rule = new cron.RecurrenceRule();
 rule.dayOfWeek = [1, 3, 5];
-rule.hour = 19;
-rule.minute = 30;
+rule.hour = 10;
+rule.minute = 0;
 var daily = cron.scheduleJob(rule, function() {
-	try {
-	var res = JSON.parse(request("GET", host + "/info.0.json").getBody().toString());
-	var str = "";
-	str += "\n**" + res.safe_title + "**\n";
-	str += "'" + res.alt + "'\n";
-	str += res.img;
-	bot.sendMessage(bot.channels[0], str);
-	} catch(err) {
-		return "Error occured! Comic could not be retrieved!"
-	}
-})
-
-chat.Add("xkcddaily", 0, function() {
 	try {
 	var res = JSON.parse(request("GET", host + "/info.0.json").getBody().toString());
 	var str = "";
