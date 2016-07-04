3d3abf79a4f0ae00c478bae84fc290bf1c4476a3
VariousMilkshakes <vm@hadesoft.io>
2016-07-04 17:13:26 +0100

    attempt 1 at plugin, external-messages

---
+++ plugins/external-interaction/main.js
@@ -0,0 +1,68 @@
+var Socket	= require('socket.io');
+
+var chat	= require("../chat/main.js");
+
+var KEY = "wazzup";
+
+var io = Socket();
+var clients = [];
+
+io.on('register', function (data){
+
+	var responce = {};
+
+	if (data.key == KEY)
+		responce = registerUser(data);
+	else
+		responce.err = "Access Key Denied";
+
+	this.to(data.cid).emit('confirm', responce);
+
+});
+
+io.on('external-message', function (packet){
+
+	if (registeredUser(packet.key)) {
+
+		bot.sendMessage(bot.channels[0], packet.message);
+		
+	}
+
+})
+
+io.listen(3000);
+
+function registerUser (user){
+
+	var clientKey = generateKey(10);
+	clients.push(clientKey);
+	return {
+		'err': false,
+		'key': clientKey
+	};
+
+}
+
+function registeredUser (key){
+
+	for (keys in clients) {
+		if (key === keys)
+			return true;
+	}
+
+	return false;
+
+}
+
+function generateKey (length){
+
+	var key = "";
+
+	for (var i = 0; i < length; i++) {
+		var ascIndx = Math.floor(Math.random() * 25) + 65;
+		key 		+= String.fromCharCode(ascIndx);
+	}
+
+	return key;
+
+}
